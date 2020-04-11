8157aa1eaa4e00f42fb0625bf955d74ba0e5f45b
havidpradipta <havid.husada@gmail.com>
2020-04-11 22:34:33 +0700

    Tugas_2721

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Jono", 19);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Joni", 50, 190104, "logical information", "Dosen@teacher.amikom.ac.id");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Havid Pradipta Husada", 19, 2721, "havid.husada@student.amikom.ac.id");
+            student.GetNameAndAge();
+        }
+    }
+}
